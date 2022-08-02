cc85125f24493a1a0f07c04dd3733cc085d5f6b3
ARAVINDH-KUMAR <aravindhvinod11@gmail.com>
2022-08-02 10:04:11 +0530

    My Second Push

--- Lambda.java
+++ Lambda.java
@@ -3,5 +3,6 @@ package com.learn.java;
 public class Lambda {
     public static void main(String[] args) {
         System.out.println("Hello Aravindh");
+        System.out.println("Git Push");
     }
 }
