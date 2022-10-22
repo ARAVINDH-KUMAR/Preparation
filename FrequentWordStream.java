417ba4c458002aa0e66728fc3886d3f5c7191d12
ARAVINDH-KUMAR <aravindhvinod11@gmail.com>
2022-10-22 09:38:59 +0530

    commiting

---
+++ FrequentWordStream.java
@@ -0,0 +1,22 @@
+package com.learn.java;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+public class FrequentWordStream {
+    public static void main(String[] args) {
+//        String string = "Big black bug bit a big black dog on his big black nose";
+//        string = string.toLowerCase();
+//        List<String> str = List.of(string.split(" "));
+//        List<String> l2 = str.stream().distinct().collect(Collectors.toList());
+//        List<String> l3 = str.stream(e -> {
+//            for (int i=0;i<str.size();i++){
+//                for (int j=i+1;j<str.size();j++){
+//                    if()
+//                }
+//            }
+//        }).collect(Collectors.toList());
+//        System.out.println(l2);
+    }
+}
---
+++ StringPool.java
@@ -0,0 +1,9 @@
+package com.learn.java;
+
+public class StringPool {
+    public static void main(String[] args) {
+        String s1 = "aravindh" ;
+        s1 = "Kumar";
+        System.out.println(s1);
+    }
+}
